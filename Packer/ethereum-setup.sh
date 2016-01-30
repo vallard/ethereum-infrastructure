7bd2627bbde5567dbd92cc19847868185913cdcc
vallard <vallard@benincosa.com>
2016-01-30 08:43:49 -0800

    more updates

--- Packer/ethereum-setup.sh
+++ Packer/ethereum-setup.sh
@@ -4,7 +4,9 @@ date >/tmp/date-of-birth
 # grab the source and build 
 git clone https://github.com/vallard/ethereum-infrastructure.git
 cd ethereum-infrastructure/Docker
-docker build -t vallard/ethereum:latest . 
+sudo docker build -t vallard/ethereum:latest . 
 # hacks to try to make the image persist after imaging. 
+echo "Restarting docker"
 sudo systemctl restart docker
+sleep 20
 docker images
