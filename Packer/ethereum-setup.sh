923e590175f302469a0823ccea1ec764c22f24cb
vallard <vallard@benincosa.com>
2016-01-30 11:03:40 -0800

    updates to make it so coreos doesn't reboot

--- Packer/ethereum-setup.sh
+++ Packer/ethereum-setup.sh
@@ -5,8 +5,15 @@ date >/tmp/date-of-birth
 git clone https://github.com/vallard/ethereum-infrastructure.git
 cd ethereum-infrastructure/Docker
 sudo docker build -t vallard/ethereum:latest . 
+sudo docker pull hello-world
 # hacks to make sure the docker image is preserved!
 echo "Restarting docker"
 sudo systemctl restart docker
 sleep 40
+
+# make it so cluster doesn't reboot
+sudo systemctl stop update-engine.service
+sudo systemctl disable update-engine.service
+sudo systemctl stop locksmithd.service
+sudo systemctl disable locksmithd.service
 docker images
