9661ac0fabac4f5e4b8faa7330b3324c8158e84e
vallard <vallard@benincosa.com>
2016-01-30 21:34:25 -0800

    updated startmining init

--- terraform/startmining.sh
+++ terraform/startmining.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# hack to turn off updates. 
+sudo systemctl stop update-engine.service
+sudo systemctl disable update-engine.service
+sudo systemctl stop locksmithd.service
+sudo systemctl disable locksmithd.service
+
 # script that runs when servers come up. 
 # spin up 2 docker containers per node for fun.
 for i in $(seq 2); do 
