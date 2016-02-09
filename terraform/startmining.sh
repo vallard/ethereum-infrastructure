5bc48f273dba590dd18fcafc36d277777181c611
vallard <vallard@benincosa.com>
2016-02-09 14:43:35 -0800

    edit to start mining

--- terraform/startmining.sh
+++ terraform/startmining.sh
@@ -8,9 +8,11 @@ sudo systemctl disable locksmithd.service
 
 # script that runs when servers come up. 
 # spin up 2 docker containers per node for fun.
-for i in $(seq 2); do 
+for i in $(seq 1); do 
   docker run -d -e 'ETH_NETWORK_ID=99991' \
               -e 'MINERTHREADS=1' \
+              -p 8545:8545 \
+              -p 30303:30303 \
               --name ethereum-0${i} \
               vallard/ethereum
 done
